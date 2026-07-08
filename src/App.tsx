495229a24281100a274386d1fec983929cae9a4d
rubengitdev <rubencahyadi504@gmail.com>
2026-07-08 19:21:06 +0700

    Initial Setup for react typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { HabitForm } from "./components/HabitForm";
+import { Header } from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <HabitForm />
+    </div>
+  );
+}
