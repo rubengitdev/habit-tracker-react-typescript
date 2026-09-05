12716ba19c1e983373f223b995206c2e1feed38e
rubengitdev <rubencahyadi504@gmail.com>
2026-09-05 23:06:10 +0700

    Bugfix: Return boolean to check duplicate habit name

--- src/context/HabitProvider.tsx
+++ src/context/HabitProvider.tsx
@@ -1,44 +1,54 @@
-import { isSameDay } from "date-fns";
-import { type ReactNode } from "react";
-import { HabitContext, type Habit } from "./useHabits";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import { isSameDay } from 'date-fns';
+import { type ReactNode } from 'react';
+import { HabitContext, type Habit } from './useHabits';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 type HabitProviderProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 export function HabitProvider({ children }: HabitProviderProps) {
-  const [habits, setHabits] = useLocalStorage<Habit[]>("Habits", []);
+    const [habits, setHabits] = useLocalStorage<Habit[]>('Habits', []);
 
-  function addHabit(name: string) {
-    setHabits((curr) => [
-      ...curr,
-      { id: crypto.randomUUID(), name, completions: [] },
-    ]);
-  }
+    function addHabit(name: string) {
+        const alreadyExists = habits.some(
+            (habit) =>
+                habit.name.trim().toLowerCase() === name.trim().toLowerCase(),
+        );
+        if (alreadyExists) {
+            return false;
+        }
+        setHabits((curr) => [
+            ...curr,
+            { id: crypto.randomUUID(), name, completions: [] },
+        ]);
+        return true;
+    }
 
-  function deleteHabit(id: string) {
-    setHabits((curr) => curr.filter((h) => h.id !== id));
-  }
+    function deleteHabit(id: string) {
+        setHabits((curr) => curr.filter((h) => h.id !== id));
+    }
 
-  function toggleHabit(id: string, date: Date) {
-    setHabits((curr) =>
-      curr.map((h) => {
-        if (h.id !== id) return h;
+    function toggleHabit(id: string, date: Date) {
+        setHabits((curr) =>
+            curr.map((h) => {
+                if (h.id !== id) return h;
 
-        const alreadyDone = h.completions.some((c) => isSameDay(c, date));
-        const completions = alreadyDone
-          ? h.completions.filter((c) => !isSameDay(c, date))
-          : [...h.completions, date];
+                const alreadyDone = h.completions.some((c) =>
+                    isSameDay(c, date),
+                );
+                const completions = alreadyDone
+                    ? h.completions.filter((c) => !isSameDay(c, date))
+                    : [...h.completions, date];
 
-        return { ...h, completions };
-      }),
+                return { ...h, completions };
+            }),
+        );
+    }
+
+    return (
+        <HabitContext value={{ habits, addHabit, toggleHabit, deleteHabit }}>
+            {children}
+        </HabitContext>
     );
-  }
-
-  return (
-    <HabitContext value={{ habits, addHabit, toggleHabit, deleteHabit }}>
-      {children}
-    </HabitContext>
-  );
 }
